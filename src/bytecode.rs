f28afaa26c385721c2aff7b219289d965215de7b
wavefnx <157986149+wavefnx@users.noreply.github.com>
2024-04-04 13:28:40 +0200

    enhance(bytecode): more precise selector extractor at code init

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -7,32 +7,42 @@ pub struct Bytecode {
 }
 
 impl Bytecode {
-    /// Finds function selectors within the bytecode.
+    /// Find selectors in the bytecode.
     ///
-    /// Scan the bytecode for specific patterns that correspond to
-    /// the start of function selectors. It returns a set of found selectors
-    /// as hexadecimal strings.
-    /// ```rs
-    /// JUMPI (0x57)
-    /// DUP1  (0x80)
-    /// PUSH4 (0x63)
-    /// ```
+    /// The bytecode pattern `PUSH4 <selector> EQ` usually occus at the initialization of the code where
+    /// the function selectors are compared to the input calldata to determine the byte offset
+    /// of the function to be executed.
+    ///
     /// Returns:
     /// A `HashSet<String>` containing the unique hexadecimal function selectors found in the bytecode.
+    ///
+    /// The exact steps depend on the compiler and version, although the general pattern is:
+    /// ```rs
+    /// // 1. Extract the function selector from the calldata and push in the stack. (mask,shr, ...)
+    /// // 2. Duplicate the selector (to be consumed by `EQ`)
+    /// DUP1
+    /// // 3. Push the function selector to compare against
+    /// PUSH4 <selector>
+    /// // 4. Compare the two selectors, push 1 if equal, 0 otherwise
+    /// EQ
+    /// // 5. Push the jump destination if the selectors match
+    /// PUSH2 <offset>
+    /// // 6. Jump to <offset> if the result of `EQ` is 1
+    /// JUMPI
+    /// ```
     #[inline]
     pub fn find_function_selectors(&self) -> HashSet<String> {
-        self.inner
-            .windows(7)
-            .filter_map(|window| {
-                // Check if the window starts with the specific pattern
-                if window.starts_with(&[0x57, 0x80, 0x63]) {
-                    // If yes, extract the next 4 bytes as the selector
-                    Some(hex::encode(&window[3..7]))
-                } else {
-                    None
-                }
-            })
-            .collect()
+        let mut selectors = HashSet::new();
+        let selector_size = 5;
+
+        for idx in 0..self.inner.len().saturating_sub(selector_size) {
+            // since we use `saturating_sub(pattern_length)` the next `pattern_length` bytes will be available
+            if self.inner[idx] == 0x63 && self.inner[idx + selector_size] == 0x14 {
+                selectors.insert(hex::encode(&self.inner[idx + 1..idx + selector_size]));
+            }
+        }
+
+        selectors
     }
 }
 
